3ed0c15450022cc5cd0e3580628904b02d08e257
Julian Hess <jhess@broadinstitute.org>
2020-01-09 23:50:04 +0000

    Use containerized workers by default

--- canine/backends/dockerTransient.py
+++ canine/backends/dockerTransient.py
@@ -19,7 +19,8 @@ import pandas as pd
 
 class DockerTransientImageSlurmBackend(TransientImageSlurmBackend): # {{{
     def __init__(
-        self, nfs_compute_script = "/usr/local/share/cga_pipeline/src/provision_storage.sh",
+        self, nfs_compute_script = "/usr/local/share/cga_pipeline/src/provision_storage_container_host.sh",
+        compute_script = "/usr/local/share/cga_pipeline/src/provision_worker_container_host.sh",
         nfs_disk_size = 100, nfs_disk_type = "pd-standard", nfs_action_on_stop = "stop",
         action_on_stop = "delete", image_family = "pydpiper", image = None,
         cluster_name = None, clust_frac = 0.01, user = os.environ["USER"], **kwargs
