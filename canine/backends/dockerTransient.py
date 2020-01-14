40727e1ac5e1f6061056ff0b137640ae9a4208c4
Julian Hess <jhess@broadinstitute.org>
2020-01-14 20:06:08 +0000

    Increase default NFS disk size for perf. reasons
    Google artificially scales disk performance by size; a 2 TB disk seems like
    a sweet spot

--- canine/backends/dockerTransient.py
+++ canine/backends/dockerTransient.py
@@ -21,7 +21,7 @@ class DockerTransientImageSlurmBackend(TransientImageSlurmBackend): # {{{
     def __init__(
         self, nfs_compute_script = "/usr/local/share/cga_pipeline/src/provision_storage_container_host.sh",
         compute_script = "/usr/local/share/cga_pipeline/src/provision_worker_container_host.sh",
-        nfs_disk_size = 100, nfs_disk_type = "pd-standard", nfs_action_on_stop = "stop",
+        nfs_disk_size = 2000, nfs_disk_type = "pd-standard", nfs_action_on_stop = "stop",
         action_on_stop = "delete", image_family = "pydpiper", image = None,
         cluster_name = None, clust_frac = 0.01, user = os.environ["USER"], **kwargs
     ):
