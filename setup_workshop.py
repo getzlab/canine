275d248734401fd2276d853891d80a4404a12ef6
Julian Hess <jhess@broadinstitute.org>
2020-07-28 18:33:39 -0400

    Update workshop setup script

---
+++ setup_workshop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import canine
+
+if __name__ == '__main__':
+    print("Provisioning workshop cluster")
+    email = canine.utils.get_gcp_username()
+    user = email.split('@')[0]
+    backend = canine.backends.TransientGCPSlurmBackend(
+        'wolf-workshop-{}'.format(user),
+        controller_type='n1-standard-2',
+        project='broad-getzlab-wolfws-202007'
+    )
+    backend.start()
+    print("Your cluster is now ready for the workshop. Please run")
+    print("gcloud compute ssh wolf-workshop-{}-controller --project broad-getzlab-wolfws-202007 \\")
+    print("  --zone us-central1-a")
+    print("to log in to the cluster.")
