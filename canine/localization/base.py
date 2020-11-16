1989f0f7be563d3a73f69f5fb07f5385bfc650cd
Julian Hess <jhess@broadinstitute.org>
2020-11-15 23:55:51 -0500

    Can't output diagnostics to stderr
    Why is this not working?

--- canine/localization/base.py
+++ canine/localization/base.py
@@ -711,7 +711,7 @@ class AbstractLocalizer(abc.ABC):
                   # wait for device to attach
                   "tries=0",
                   "while [ ! -b /dev/disk/by-id/google-{disk_name} ]; do".format(disk_name = disk),
-                  '[ $tries -gt 12 ] && { echo "Timeout exceeded for disk to attach; perhaps the stderr of `gcloud compute instances attach disk` might contain insight:"; cat $DIAG_FILE; exit 1; } > /dev/stderr',
+                  '[ $tries -gt 12 ] && { echo "Timeout exceeded for disk to attach; perhaps the stderr of `gcloud compute instances attach disk` might contain insight:"; cat $DIAG_FILE; exit 1; }',
                   "sleep 10; ((tries++))",
                   "done",
 
@@ -725,7 +725,8 @@ class AbstractLocalizer(abc.ABC):
 
                   # because we forced zero exits for the previous commands,
                   # we need to verify that the mount actually exists
-                  "mountpoint -q $CANINE_LOCAL_DISK_DIR || { echo 'Read-only disk mount failed!'; cat $DIAG_FILE; exit 1; } > /dev/stderr",
+                  "mountpoint -q $CANINE_LOCAL_DISK_DIR || { echo 'Read-only disk mount failed!'; cat $DIAG_FILE; exit 1; }",
+                  # TODO: write error to stderr; this isn't working for some reason
 
                   # symlink into the canine directory
                   # note it might already exist if we are retrying this task
