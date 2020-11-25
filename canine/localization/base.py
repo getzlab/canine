62b11dc20786c6a83f8a25a2686f734722f7c8ef
Jialin Ma <jma@broadinstitute.org>
2020-11-24 22:40:53 -0500

    Implement via find

--- canine/localization/base.py
+++ canine/localization/base.py
@@ -772,7 +772,7 @@ class AbstractLocalizer(abc.ABC):
         
         ## Symlink common inputs to job inputs
         localization_tasks += [
-            '(ls -d "$CANINE_COMMON"/* 2>/dev/null || true) | xargs -L1 -I{} ln -s {} "$CANINE_JOB_INPUTS"/'
+            'find "$CANINE_COMMON"/ -mindepth 1 -maxdepth 1 -exec ln -sf {} "$CANINE_JOB_INPUTS"/ \;'
         ]
 
         # generate setup script
