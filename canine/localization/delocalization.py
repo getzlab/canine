d7ea6dbd53a9b66c4f8c7e41796a7a5e82e5f2b9
Julian Hess <jhess@broadinstitute.org>
2020-12-19 08:31:37 +0000

    Compute checksums in one batch

--- canine/localization/delocalization.py
+++ canine/localization/delocalization.py
@@ -27,18 +27,24 @@ def same_volume(a, b):
     )
     return len(set(vols.decode("utf-8").rstrip().split("\n"))) == 1
 
-def get_gsfile_crc32c(path):
-    """
-    From wolf.Task
-    """
-    cmd = 'echo "Computing CRC32C checksum for {0} ..." >&2; gsutil hash -c -h {0}'.format(shlex.quote(path))
-    output = subprocess.check_output(cmd, shell=True).decode()
-    output = output.rstrip().split("\n")
-    output = [o for o in output if re.search(r"^\tHash \(crc32c\):\t\t", o)]
-    assert len(output) == 1
-    output = output[0]
-    output = re.sub(r"^\tHash \(crc32c\):\t\t", "", output)
-    return output
+def compute_crc32c(direc):
+    p = subprocess.Popen(
+      # -mindepth 2 to avoid hashing stdout/stderr; ! -name ".*" to avoid hashing
+      # any preexisting crc32c files
+      'find {} -mindepth 2 ! -type d ! -name ".*" | xargs gsutil hash -ch'.format(direc),
+      shell = True,
+      stdout = subprocess.PIPE,
+      stderr = subprocess.PIPE,
+      text = True
+    )
+    out, err = p.communicate()
+
+    if p.returncode != 0:
+        print("Error computing checksums, see stderr for details:", file = sys.stderr, flush = True)
+        print(err, file = sys.stderr, flush = True)
+        return []
+    else:
+        return re.findall(r'Hashes \[hex\] for (.*):\n\tHash \(crc32c\):\t\t([A-F0-9]{8})\n', out)
 
 def main(output_dir, jobId, patterns, copy):
     jobdir = os.path.join(output_dir, str(jobId))
@@ -69,23 +75,6 @@ def main(output_dir, jobId, patterns, copy):
                     else:
                         shutil.copytree(target, dest)
 
-                # compute checksum
-                if name not in {'stdout', 'stderr'}:
-                    try:
-                        crc32 = get_gsfile_crc32c(target)
-                        with open(
-                          os.path.join(
-                            jobdir,
-                            name,
-                            os.path.dirname(os.path.relpath(target)),
-                            "." + os.path.basename(target) + ".crc32c"
-                          ),
-                          "w"
-                        ) as crc32_file:
-                            crc32_file.write(crc32 + "\n")
-                    except subprocess.CalledProcessError:
-                        print("Error computing checksum for {}!".format(target), file = sys.stderr)
-
                 # write job manifest
                 manifest.write("{}\t{}\t{}\t{}\n".format(
                     jobId,
@@ -105,6 +94,18 @@ def main(output_dir, jobId, patterns, copy):
                     pattern,
                     "//not_found"
                 ))
+            else:
+                print('INFO: matched output name "{0}" (pattern "{1}")'.format(name,  pattern), file = sys.stderr)
+
+    # compute checksums for all files
+    print('Computing CRC32C checksums ...', file = sys.stderr, flush = True, end = "")
+    for target, crc32c in compute_crc32c(jobdir):
+        with open(os.path.join(
+            os.path.dirname(target),
+            "." + os.path.basename(target) + ".crc32c"
+          ), "w") as crc32c_file:
+            crc32c_file.write(crc32c + "\n")
+    print(' done', file = sys.stderr, flush = True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('canine-delocalizer')
