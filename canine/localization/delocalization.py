096419bca8fe74bc946f126e587b403d507e8b96
Zach Everton <evertonzachary@gmail.com>
2025-06-04 16:54:31 +0000

    symlink retries

--- canine/localization/delocalization.py
+++ canine/localization/delocalization.py
@@ -78,65 +78,85 @@ def compute_crc32c(path, fast = False):
 
     return hash_alg.hexdigest().decode().upper()
 
-def main(output_dir, jobId, patterns, copy, scratch, finished_scratch):
+def safe_symlink(num_retries=2, *symlink_args):
+    num_tries = 0
+    for t in range(num_retries):
+        try:
+            os.symlink(*symlink_args)
+            return
+        except:
+            time.sleep(5)
+    os.symlink(**symlink_args)
+
+
+def main(output_dir, jobId, patterns, copy, scratch, finished_scratch, max_retries):
     jobdir = os.path.join(output_dir, str(jobId))
     if not os.path.isdir(jobdir):
         os.makedirs(jobdir)
     matched_files = []
+    n_retries = 0
     with open(os.path.join(jobdir, '.canine_job_manifest'), 'w') as manifest:
-        for name, pattern in patterns:
-            n_matched = 0
-            for target in glob.iglob(pattern):
-                # construct output file path
-                if name in {'stdout', 'stderr'}:
-                    dest = os.path.join(jobdir, name)
-                else:
-                    dest = os.path.join(jobdir, name, os.path.relpath(target))
-
-                # populate output directory
-                if not os.path.lexists(dest):
-                    if not os.path.isdir(os.path.dirname(dest)):
-                        os.makedirs(os.path.dirname(dest))
-                    # we've output to a scratch disk; create (broken) symlink to RODISK mount
-                    if scratch and name not in copy:
-                        os.symlink(os.path.abspath(target), dest)
-                    # Same volume check catches outputs from outside the workspace
-                    elif name in copy or not same_volume(target, jobdir):
-                        print(f'INFO: copying (not symlinking) file \'{target}\' (name "{name}", pattern "{pattern}")', file = sys.stderr)
-                        if os.path.isfile(target):
-                            shutil.copyfile(os.path.abspath(target), dest)
-                        else:
-                            shutil.copytree(target, dest)
-                    # TODO: is st_dev check equivalent to same_volume?
-                    elif os.stat(target).st_dev == os.stat(os.path.dirname(dest)).st_dev:
-                        os.symlink(os.path.relpath(target, os.path.dirname(dest)), dest)
+        while len(retry_patterns) > 0:
+            retry_patterns = []
+            for name, pattern in patterns:
+                n_matched = 0
+                for target in glob.iglob(pattern):
+                    # construct output file path
+                    if name in {'stdout', 'stderr'}:
+                        dest = os.path.join(jobdir, name)
                     else:
-                        os.symlink(os.path.abspath(target), dest)
+                        dest = os.path.join(jobdir, name, os.path.relpath(target))
 
-                # write job manifest
-                manifest.write("{}\t{}\t{}\t{}\n".format(
-                    jobId,
-                    name,
-                    pattern,
-                    os.path.relpath(dest.strip(), output_dir)
-                ))
+                    # populate output directory
+                    if not os.path.lexists(dest):
+                        if not os.path.isdir(os.path.dirname(dest)):
+                            os.makedirs(os.path.dirname(dest))
+                        # we've output to a scratch disk; create (broken) symlink to RODISK mount
+                        if scratch and name not in copy:
+                            safe_symlink(max_retries, os.path.abspath(target), dest)
+                        # Same volume check catches outputs from outside the workspace
+                        elif name in copy or not same_volume(target, jobdir):
+                            print(f'INFO: copying (not symlinking) file \'{target}\' (name "{name}", pattern "{pattern}")', file = sys.stderr)
+                            if os.path.isfile(target):
+                                shutil.copyfile(os.path.abspath(target), dest)
+                            else:
+                                shutil.copytree(target, dest)
+                        # TODO: is st_dev check equivalent to same_volume?
+                        elif os.stat(target).st_dev == os.stat(os.path.dirname(dest)).st_dev:
+                            safe_symlink(max_retries, os.path.relpath(target, os.path.dirname(dest)), dest)
+                        else:
+                            safe_symlink(max_retries, os.path.abspath(target), dest)
 
-                # append to array of files that will be hashed
-                matched_files.append([dest, target])
+                    # write job manifest
+                    manifest.write("{}\t{}\t{}\t{}\n".format(
+                        jobId,
+                        name,
+                        pattern,
+                        os.path.relpath(dest.strip(), output_dir)
+                    ))
 
-                n_matched += 1
+                    # append to array of files that will be hashed
+                    matched_files.append([dest, target])
 
-            # warn if no files matched; make log in manifest
-            if n_matched == 0:
-                print('WARNING: output name "{0}" (pattern "{1}") not found.'.format(name,  pattern), file = sys.stderr)
-                manifest.write("{}\t{}\t{}\t{}\n".format(
-                    jobId,
-                    name,
-                    pattern,
-                    "//not_found"
-                ))
-            else:
-                print('INFO: matched output name "{0}" (pattern "{1}")'.format(name,  pattern), file = sys.stderr)
+                    n_matched += 1
+
+                # warn if no files matched; make log in manifest
+                if n_matched == 0:
+                    if n_retries < max_retries:
+                        retry_patterns.append([name,pattern])
+                    else:
+                        print('WARNING: output name "{0}" (pattern "{1}") not found.'.format(name,  pattern), file = sys.stderr)
+                        manifest.write("{}\t{}\t{}\t{}\n".format(
+                            jobId,
+                            name,
+                            pattern,
+                            "//not_found"
+                        ))
+                else:
+                    print('INFO: matched output name "{0}" (pattern "{1}")'.format(name,  pattern), file = sys.stderr)
+            patterns = retry_patterns
+            n_retries+=1
+            time.sleep(5)
 
     # compute checksums for all files, if job exited successfully
     if os.environ["CANINE_JOB_RC"] == "0":
